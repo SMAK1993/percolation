a338bbfe30537591c55caa1691267b96e78cb79e
Syed Mohammad Adnan Karim <SMAK1993@users.noreply.github.com>
2018-02-25 18:49:01 -0500

    open and isOpen methods completed

--- Percolation.java
+++ Percolation.java
@@ -8,13 +8,15 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     boolean[][] sites;
+    WeightedQuickUnionUF myWeightedQuickUnionUF;
+    int sitesLength;
 
     // create n-by-n grid, with all sites blocked
     public Percolation(int n) {
-        if (n <= 0) {
-            throw new IllegalArgumentException(Integer.toString(n));
-        }
+        if (n <= 0) throw new IllegalArgumentException(Integer.toString(n));
         sites = new boolean[n][n];
+        myWeightedQuickUnionUF = new WeightedQuickUnionUF(n*n);
+        sitesLength = n - 1;
         return;
     }
 
@@ -22,7 +24,31 @@ public class Percolation {
     public void open(int row, int col) {
         checkArgument(row);
         checkArgument(col);
-        sites[row-1][col-1] = true;
+        sites[row - 1][col - 1] = true;
+        if (row == 0) {
+            if (isOpen(row + 1, col)) myWeightedQuickUnionUF.union(indexValue(row, col), indexValue(row + 1, col));
+            if (col == 0 && isOpen(row, col + 1)) myWeightedQuickUnionUF.union(indexValue(row, col), indexValue(row, col + 1));
+            else if (col == sitesLength && isOpen(row ,col - 1)) myWeightedQuickUnionUF.union(indexValue(row, col), indexValue(row, col - 1));
+            else {
+                if (isOpen(row, col + 1)) myWeightedQuickUnionUF.union(indexValue(row, col), indexValue(row, col + 1));
+                if (isOpen(row, col - 1)) myWeightedQuickUnionUF.union(indexValue(row, col), indexValue(row, col - 1));
+            }
+        }
+        else if (row == sitesLength) {
+            if (isOpen(row - 1, col)) myWeightedQuickUnionUF.union(indexValue(row, col), indexValue(row - 1, col));
+            if (col == 0 && isOpen(row, col + 1)) myWeightedQuickUnionUF.union(indexValue(row, col), indexValue(row, col + 1));
+            else if (col == sitesLength && isOpen(row, col - 1)) myWeightedQuickUnionUF.union(indexValue(row, col), indexValue(row, col - 1));
+            else {
+                if (isOpen(row, col + 1)) myWeightedQuickUnionUF.union(indexValue(row, col), indexValue(row, col + 1));
+                if (isOpen(row, col - 1)) myWeightedQuickUnionUF.union(indexValue(row, col), indexValue(row, col - 1));
+            }
+        }
+        else {
+            if (isOpen(row, col + 1)) myWeightedQuickUnionUF.union(indexValue(row, col), indexValue(row, col + 1));
+            if (isOpen(row, col - 1)) myWeightedQuickUnionUF.union(indexValue(row, col), indexValue(row, col - 1));
+            if (isOpen(row + 1, col)) myWeightedQuickUnionUF.union(indexValue(row, col), indexValue(row + 1, col));
+            if (isOpen(row - 1, col)) myWeightedQuickUnionUF.union(indexValue(row, col), indexValue(row - 1, col));
+        }
         return;
     }
 
@@ -30,7 +56,7 @@ public class Percolation {
     public boolean isOpen(int row, int col) {
         checkArgument(row);
         checkArgument(col);
-        return sites[row-1][col-1];
+        return sites[row - 1][col - 1];
     }
 
     // is site (row, col) full?
@@ -54,18 +80,22 @@ public class Percolation {
         }
     }
 
+    public int indexValue(int row, int col) {
+        return ((sitesLength * row) + col);
+    }
+
     public void printSites() {
         // System.out.println(Arrays.deepToString(myPercolation.sites));
+        System.out.println("Printing Sites");
         for (boolean[] site : sites) {
             System.out.println(Arrays.toString(site));
         }
-        System.out.println();
     }
 
     // test client (optional)
     public static void main(String[] args) {
-        System.out.println("Starting Percolation");
         Percolation myPercolation = new Percolation(3);
+        System.out.println("N = " + myPercolation.sites.length);
         myPercolation.printSites();
         myPercolation.open(2, 2);
         myPercolation.printSites();
